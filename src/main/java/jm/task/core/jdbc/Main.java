2567a4fc5d05787ff7270c77c4df286aa6be9353
enoti <enotishe314@gmail.com>
2023-03-10 13:32:36 +0300

    sd

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,16 +9,16 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        userService.createUsersTable();
-
+//        userService.createUsersTable();
+//
         userService.saveUser("Name1", "LastName1", (byte) 20);
         userService.saveUser("Name2", "LastName2", (byte) 25);
         userService.saveUser("Name3", "LastName3", (byte) 31);
         userService.saveUser("Name4", "LastName4", (byte) 38);
-
-        userService.removeUserById(1);
-        System.out.println(userService.getAllUsers());
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+//
+//        userService.removeUserById(1);
+//        System.out.println(userService.getAllUsers());
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
     }
 }
